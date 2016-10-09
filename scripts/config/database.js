95c0150c856caac578f0957f4077224eb616d8dc
louisguitton <louisguitton93@gmail.com>
2016-10-10 00:10:44 +0100

    refresh app

--- scripts/config/database.js
+++ scripts/config/database.js
@@ -1,5 +1,5 @@
 // config/database.js
 
-var url = 'mongodb://root:admin@ds055762.mongolab.com:55762/battledb';
+var url = 'mongodb://root:admin@ds055762.mlab.com:55762/battledb';
 
 module.exports.url = url;
